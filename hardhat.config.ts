e424246bcfd82ba53e9eedfee81c8acf72728543
0xdefilover <13178177+0xdefilover@users.noreply.github.com>
2022-11-03 10:36:53 +0800

    add gas report config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
     },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
   },
 };
 
